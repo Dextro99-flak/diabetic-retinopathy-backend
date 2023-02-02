df609bfc314f89821b3429cc5c1713fe2170bc7c
burhanuddin unwalla <burhanunwalla@gmail.com>
2023-02-02 16:12:08 +0530

    i m noob

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template,flash,redirect
 import config
+from forms import LoginForm
 
 #app = Flask(__name__)
 
@@ -10,5 +11,13 @@ app.add_api(config.basedir/'swagger.yml')
 def home():
 	return render_template('home.html')
 
+@app.route('/login',methods=['GET','POST'])
+def login():
+	form=LoginForm()
+	if form.validate_on_submit():
+		flash('login requested for user {}, remember me={}'.format(form.username.data,form.remember_me.data))
+		return redirect(url_for('home'))
+	return render_template('login.html',title='sign in',form=form)
+
 if __name__=='__main__':
-	app.run(host='0.0.0.0', port=8000, debug=True)
+	app.run( debug=True)
--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import pathlib
 import connexion
 from flask_sqlalchemy import SQLAlchemy
+import os
+from flask import LoginManager
+from flask_login import UserMixin
 
 basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = connexion.App(__name__, specification_dir=basedir)
@@ -8,7 +11,9 @@ connex_app = connexion.App(__name__, specification_dir=basedir)
 app = connex_app.app
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{basedir}/analysis.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'you will never guess ha ha'  #for user login
 
+login=LoginManager(app)
 db = SQLAlchemy(app)
 
 class Analysis(db.Model):
@@ -20,3 +25,17 @@ class Analysis(db.Model):
 	ex_filename = db.Column(db.String, nullable=True)
 	se_filename = db.Column(db.String, nullable=True)
 	ma_filename = db.Column(db.String, nullable=True)
+	user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+
+class User(UserMixin, db.Model):   
+	id = db.Column(db.Integer,primary_key=True)
+	username = db.Column(db.String(64),index=True,unique=True)
+	email = db.Column(db.String(120), index=True, unique=True)
+	password_hash = db.Column(db.String(128))
+
+	def __repr__(self):
+		return '<user: {}>'.format(self.username)
+
+@login.user_loader
+def load_user(id):
+	return User.query.get(int(id))
