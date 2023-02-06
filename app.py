f1787cbcb7383510d7119bdacd577fad39fd42f3
burhanuddin unwalla <burhanunwalla@gmail.com>
2023-02-06 19:01:40 +0530

    user authentication added

--- app.py
+++ app.py
@@ -1,23 +1,56 @@
-from flask import render_template,flash,redirect
+from flask import render_template,flash,redirect,url_for,request
 import config
 from forms import LoginForm
+from flask_login import current_user,login_user,logout_user,login_required
+from config import User,Analysis,db
+from werkzeug.urls import url_parse
+
 
 #app = Flask(__name__)
 
 app = config.connex_app
 app.add_api(config.basedir/'swagger.yml')
+application = app.app
 
 @app.route('/')
 def home():
+	print(current_user)
 	return render_template('home.html')
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
 	form=LoginForm()
 	if form.validate_on_submit():
-		flash('login requested for user {}, remember me={}'.format(form.username.data,form.remember_me.data))
-		return redirect(url_for('home'))
-	return render_template('login.html',title='sign in',form=form)
+		user=User.query.filter_by(username=form.username.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash('invalid username or password')
+			return redirect(url_for('login'))
+		login_user(user, remember=form.remember_me.data)
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc != '':
+			next_page=url_for('home')
+		return redirect((next_page))
+	return render_template('login.html',title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('home'))
+
+@app.route('/detector')
+@login_required
+def detector():
+	return render_template('detector.html')
+
+
+
+#makes interacting with db from shell easier
+#@app.shell_context_processor
+#def make_shell_context():
+#	return {'db': db,'User':User,'Analysis':Analysis}
 
 if __name__=='__main__':
 	app.run( debug=True)
+
--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import pathlib
 import connexion
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 import os
-from flask import LoginManager
-from flask_login import UserMixin
+from flask_login import UserMixin,LoginManager
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_migrate import Migrate
 
 basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = connexion.App(__name__, specification_dir=basedir)
@@ -13,8 +15,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{basedir}/analysis.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'you will never guess ha ha'  #for user login
 
+convention = {
+  "ix": "ix_%(column_0_label)s",
+  "uq": "uq_%(table_name)s_%(column_0_name)s",
+  "ck": "ck_%(table_name)s_%(constraint_name)s",
+  "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+  "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+
 login=LoginManager(app)
-db = SQLAlchemy(app)
+login.login_view = 'login'
+db = SQLAlchemy(app,metadata=metadata)
+migrate = Migrate(app,db)
+
 
 class Analysis(db.Model):
 	__tablename__='analysis'
@@ -32,9 +47,16 @@ class User(UserMixin, db.Model):
 	username = db.Column(db.String(64),index=True,unique=True)
 	email = db.Column(db.String(120), index=True, unique=True)
 	password_hash = db.Column(db.String(128))
+	analysis = db.relationship('Analysis',backref='author',lazy='dynamic')
 
 	def __repr__(self):
 		return '<user: {}>'.format(self.username)
+	
+	def set_password(self,password):
+		self.password_hash=generate_password_hash(password)
+	
+	def check_password(self,password):
+		return check_password_hash(self.password_hash,password)
 
 @login.user_loader
 def load_user(id):
